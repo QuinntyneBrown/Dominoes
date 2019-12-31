05b4fba521da27782675fdba7ddf744278373112
Quinntyne Brown <Quinntyne.Brown@trader.ca>
2019-12-30 23:32:00 -0500

    add player and model

---
+++ src/Dominoes.Core/Model/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dominoes.Core.Model
+{
+    public class Game
+    {
+        public Guid GameId { get; set; }
+        public ICollection<Player> Players { get; set; }
+    }
+}
---
+++ src/Dominoes.Core/Model/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dominoes.Core.Model
+{
+    public class Player
+    {
+        public Guid PlayerId { get; set; }
+        public string Name { get; set; }
+    }
+}
